de6370f60c155ef41f13d46573e8283887002f53
Kizman Jozsef <jozsef.kizman@binx.hu>
2025-01-16 16:55:17 +0100

    frontend rm webAclId

--- cdk/lib/constructs/frontend.ts
+++ cdk/lib/constructs/frontend.ts
@@ -21,7 +21,8 @@ import * as targets from "aws-cdk-lib/aws-route53-targets";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
 
 export interface FrontendProps {
-  readonly webAclId: string;
+  //readonly webAclId: string;
+  readonly enableMistral: boolean;
   readonly accessLogBucket?: IBucket;
   readonly enableIpV6: boolean;
   /** 
@@ -102,7 +103,7 @@ export class Frontend extends Construct {
         logBucket: props.accessLogBucket,
         logFilePrefix: "Frontend/",
       }),
-      webAclId: props.webAclId,
+      //webAclId: props.webAclId,
       enableIpv6: props.enableIpV6,
     });
 
