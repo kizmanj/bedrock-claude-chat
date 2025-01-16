7b0ebbaa457bfca3b86d582a24253c5041280a96
Kizman Jozsef <jozsef.kizman@binx.hu>
2025-01-16 16:51:09 +0100

    mod props

--- cdk/lib/bedrock-chat-stack.ts
+++ cdk/lib/bedrock-chat-stack.ts
@@ -28,7 +28,7 @@ export interface BedrockChatStackProps extends StackProps {
   readonly envName: string;
   readonly envPrefix: string;
   readonly bedrockRegion: string;
-  readonly webAclId: string;
+  //readonly webAclId: string;
   readonly identityProviders: TIdentityProvider[];
   readonly userPoolDomainPrefix: string;
   readonly publishedApiAllowedIpV4AddressRanges: string[];
